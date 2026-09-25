62747887eaabe8bdb5270a2a87a5a502972f1e7d
MorningStar-lab <rehmantraders550@gmail.com>
2026-09-25 16:37:11 -0700

    Add start-a-job.tsx entry

---
+++ src/start-a-job.tsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import StartJobPage from './StartJobPage';
+import './index.css';
+import './interior.css';
+import './wave3.css';
+createRoot(document.getElementById('root')!).render(<StrictMode><StartJobPage /></StrictMode>);
